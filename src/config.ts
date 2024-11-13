8dfa8df3addc65cf3424545ab72296697eeef816
Carlos Soto <csoto@spensiones.cl>
2024-11-13 10:06:15 -0300

    Camnbio de username y puert 3001

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const configuration = {
-    username: process.env.USERNAME || 'default',
+    username: process.env.USERNAME || 'Carlos Soto Gower',
     apikey: process.env.APIKEY || 'default-key',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3001,
     jwtSecretKey: process.env.JWT_SECRET_KEY || 'default-secret-key',
 }
